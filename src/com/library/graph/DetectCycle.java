74fc81b7308d0e5d085e11474fa9264e04bfa3ce
Rupak Bag <rupak.bag@gmail.com>
2017-10-21 21:01:25 -0400

    Graph Cycle test code

---
+++ src/com/library/graph/DetectCycle.java
@@ -0,0 +1,16 @@
+package com.library.graph;
+
+public class DetectCycle {
+    /*
+    * This method returns the list of nodes that form a cycle
+    * Returns Null if no cycle exists
+     */
+    public static String getCycle(Graph g) {
+        //get starting node
+        //start DFS. get adj nodes and keep a map of nodes and path
+        
+        Node n = g.nodeIterator().next();
+
+        return "";
+    }
+}
