5b83cbc0e2810ea8eeb36de810249d014c3e33cb
Rupak Bag <rupak.bag@gmail.com>
2017-11-16 21:51:28 -0500

    MovingOffice

--- src/com/library/dynamicprogramming/MovingOffice.java
+++ src/com/library/dynamicprogramming/MovingOffice.java
@@ -17,54 +17,33 @@ public class MovingOffice {
         this.sf = sf;
         this.movingCost = movingCost;
         this.optLoc = new LOC[ny.length];
-        this.optValues = new int[ny.length][4];
+        this.optValues = new int[ny.length][2];
     }
 
     public int getMovingCost() {
-        optValues[0][0] = ny[0];
-        optValues[0][1] = ny[0];
-        optValues[0][2] = sf[0];
-        optValues[0][3] = sf[0];
-
-        for (int month = 1; month < ny.length; month++) {
-            _opt(month, LOC.NY);
-            _opt(month, LOC.SF);
+        for (int month = 0; month < ny.length; month++) {
+            optValues[month][0] = _opt(month, LOC.NY);
+            optValues[month][1] = _opt(month, LOC.SF);
         }
-//        int cost = _opt(ny.length - 1, null);
-//        printOptimalLocation(ny.length - 1, null);
+        printOptimalLocation();
         return Math.min(optValues[ny.length - 1][0], optValues[sf.length - 1][1]);
     }
 
-//    private void printOptimalLocation(int month, LOC nextLoc) {
-//        if (month == 0) {
-//            if (totalCost(0, LOC.NY, nextLoc) < totalCost(0, LOC.SF, nextLoc)) System.out.println("NY");
-//            else System.out.println("SF");
-//            return;
-//        }
-//
-//        if (optValues[month - 1]) {
-//            System.out.println("NY");
-//            printOptimalLocation(month + 1, LOC.NY);
-//            return;
-//        }
-//        System.out.println("SF");
-//        printOptimalLocation(month + 1, LOC.SF);
-//    }
+    private void printOptimalLocation() {
+        for (int month = 0; month < ny.length; month++) {
+            System.out.println((optValues[month][0] < optValues[month][1]) ? "NY " : "SF ");
+        }
+    }
 
     private int _opt(int month, LOC currentLoc) {
-//        int locIndex = (currentLoc == LOC.NY)? 0 : 1;
-//        if (optValues[month][locIndex] != 0) return optValues[month][locIndex];
+        if (month == 0) return locationCost(month, currentLoc);
+        int locIndex = (currentLoc == LOC.NY)? 0 : 1;
+        if (optValues[month][locIndex] != 0) {
+            return optValues[month][locIndex];
+        }
+            return Math.min(_opt(month - 1, currentLoc) + locationCost(month, currentLoc),
+            _opt(month - 1, otherLocation(currentLoc)) + locationCost(month, currentLoc) + movingCost);
 
-        if (currentLoc == LOC.NY) {
-            optValues[month][0] = _opt(month - 1, LOC.NY) + ny[month];
-            optValues[month][1] = _opt(month - 1, LOC.SF) + ny[month] + movingCost;
-        }
-        else {
-            optValues[month][2] = _opt(month - 1, LOC.SF) + sf[month];
-            optValues[month][3] = _opt(month - 1, LOC.NY) + sf[month] + movingCost;
-        }
-        int minCost = Math.min(Math.min(optValues[month][0], optValues[month][1]), Math.min(optValues[month][2], optValues[month][3]));
-        return minCost;
     }
 
     private LOC otherLocation(LOC currentLoc) {
--- test/com/library/dynamicprogramming/MovingOfficeTest.java
+++ test/com/library/dynamicprogramming/MovingOfficeTest.java
@@ -7,8 +7,16 @@ public class MovingOfficeTest {
     MovingOffice mo;
     @Test
     public void testMovingOffice(){
-        int[] NY = new int[]{2,2,1,2};
-        int[] SF = new int[]{1,1,2,1};
+        int[] NY = new int[]{10,5,7,9};
+        int[] SF = new int[]{50,2,2,2};
+        mo = new MovingOffice(NY, SF, 10);
+        Assert.assertEquals(26, mo.getMovingCost());
+    }
+
+    @Test
+    public void testMovingOffice1(){
+        int[] NY = new int[]{1,3,20,30};
+        int[] SF = new int[]{50,20,2,4};
         mo = new MovingOffice(NY, SF, 10);
         Assert.assertEquals(20, mo.getMovingCost());
     }
