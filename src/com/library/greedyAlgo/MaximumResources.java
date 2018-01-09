d62b1fcd23b8b9ef165d743c04dda20e29c31c41
Rupak Bag <rupak.bag@gmail.com>
2018-01-09 00:02:25 -0500

    Maximum resource count algo description

--- src/com/library/greedyAlgo/MaximumResources.java
+++ src/com/library/greedyAlgo/MaximumResources.java
@@ -3,8 +3,16 @@ package com.library.greedyAlgo;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class MaximumResources {
+/*
+* Algorithm assumes a sorted array of jobs is provided (sort based on finishing times)
+* Algorithm outputs the minimum number of resources needed to execute all jobs.
+* A queue is maintained to keep all overlapping jobs. Any new job to be added checks for its overlap with the
+* first job in the queue (peek). If no overlap, the head is removed. The new job is then added and we maintain a
+* variable that stores the maximum queue size seen so far. After all jobs are processed, max queue size seen is
+* the number of resources needed. O(n)
+ */
 
+public class MaximumResources {
     public static int getMaxResources(JOB[] jobs) {
         Queue<JOB> jobQueue = new LinkedList<>();
         int maxQueueSize = 0;
--- test/com/library/greedyAlgo/MaximumResourcesTest.java
+++ test/com/library/greedyAlgo/MaximumResourcesTest.java
@@ -6,7 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class MaximumResourcesTest {
-    JOB j1, j2, j3, j4, j5, j6;
+    JOB j1, j2, j3, j4, j5, j6, j7, j8;
     MaximumResources.JOB[] jobs;
     @Before
     public void setup(){
@@ -15,7 +15,9 @@ public class MaximumResourcesTest {
         j3 = new JOB(4,6);
         j4 = new JOB(5,7);
         j5 = new JOB(5,9);
-        j6 = new JOB(9,10);
+        j6 = new JOB(9,11);
+        j7 = new JOB(10,12);
+        j8 = new JOB(10,13);
     }
 
     @Test
@@ -37,5 +39,16 @@ public class MaximumResourcesTest {
         jobs[1] = j3;
         jobs[2] = j6;
         Assert.assertEquals(1, MaximumResources.getMaxResources(jobs));
+    }
+
+    @Test
+    public void multiple_finished_job_before_max_depth(){
+        jobs = new MaximumResources.JOB[5];
+        jobs[0] = j1;
+        jobs[1] = j2;
+        jobs[2] = j6;
+        jobs[3] = j7;
+        jobs[4] = j8;
+        Assert.assertEquals(3, MaximumResources.getMaxResources(jobs));
     }
 }
